470d0b99d05ecc03ed171f78308d2078093ee8c4
377Nika2004 <borcovaveronika337@gmail.com>
2024-12-12 22:49:14 +0300

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,71 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        // Задача 1
+        int[] firstArray = new int[] {1,2,3} ;
+        double[] secondArray = new double[] {1.57, 7.654, 9.986};
+        char[] thirdArray = new char[] {'o','w','t'};
+
+        // Задача 2
+        System.out.println("Задача 2");
+        for (int i = 0; i < firstArray.length; i++) {
+            System.out.print(firstArray[i]);
+
+            if (i != firstArray.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        for (int i = 0; i < secondArray.length; i++) {
+            System.out.print(secondArray[i]);
+
+            if (i != secondArray.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        for (int i = 0; i < thirdArray.length; i++) {
+            System.out.print(thirdArray[i]);
+
+            if (i != thirdArray.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        // Задача 3
+        System.out.println();
+        System.out.println("Задача 3");
+        for (int i = firstArray.length - 1; i >= 0; i--) {
+            System.out.print(firstArray[i]);
+
+            if (i != 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        for (int i = secondArray.length - 1; i >= 0; i--) {
+            System.out.print(secondArray[i]);
+
+            if (i != 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        for (int i = thirdArray.length - 1; i >= 0; i--) {
+            System.out.print(thirdArray[i]);
+
+            if (i != 0) {
+                System.out.print(", ");
+            }
+        }
+        // Задача 4
+        System.out.println();
+        System.out.println("Задача 4");
+        for (int i = 0; i < firstArray.length; i++) {
+            if (firstArray[i] % 2 != 0){
+                firstArray[i]++;
+            }
+        }
+        System.out.println(Arrays.toString(firstArray));
     }
 }
